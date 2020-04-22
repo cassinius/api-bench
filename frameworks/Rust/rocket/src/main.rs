c60bfd65157d51e27cb3186ea75d95e35fd9d68a
Bernd Malle <bernd.malle@gmail.com>
2020-04-22 23:36:07 +0800

    seems we got rocket.rs to connect to Postgres, but the whole Struct/Impl/Macro-Style setup for `type guards` etc. makes my head spin - and the tutorials are months / years apart and totally incomplete

--- frameworks/Rust/rocket/src/main.rs
+++ frameworks/Rust/rocket/src/main.rs
@@ -1,38 +1,47 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::databases::diesel;
+
+#[database("pg_conn")]
+struct RetailerDbConn(diesel::PgConnection);
+
 // OLD Rust (2015??)
 // #[macro_use] extern crate rocket;
-use rocket::*;
+// use rocket::*;
 // use diesel::prelude::*;
 use diesel::{Queryable, Insertable};
 
-/// Needs `features = ["derive"]` in Cargo.toml
-/// sometimes seems to need json_serde as well, or not, or yes, 
-/// depending on cargo's build mood (check with `cargo mood` ;-)
-use serde::{Serialize, Deserialize};
 use rocket_contrib::json::Json;
+/// Needs `features = ["derive"]` in Cargo.toml
+/// sometimes seems to need json_serde as well, or not, or yes,
+/// depending on cargo's build mood (check with `cargo mood` ;-)
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct StatusMsg {
     status: u16,
-    msg: String
+    msg: String,
 }
 
-
+// mod schema;
 // use super::schema::people;
 #[derive(Queryable, Serialize, Deserialize)]
 pub struct Retailer {
-    pub id: i32,
-    pub GSTIN: String,
-    pub Business_name: String,
-    pub Contact_person: String,
-    pub Contact_number: i32,
-    pub Contact_address: String,
-    pub Contact_emailId: String,
-    pub Status: String,
-    pub Outlet_limit: i32,
+		pub id: i32,
+		pub GSTIN: String,
+		pub Business_name: String,
+		pub Contact_person: String,
+		pub Contact_number: i32,
+		pub Contact_address: String,
+		pub Contact_emailId: String,
+		pub Status: String,
+		pub Outlet_limit: i32,
 }
-
 
 
 /// The route only exists when type constraints are fulfilled!
@@ -46,9 +55,9 @@ fn hello(name: String, age: u8) -> String {
 
 #[get("/api")]
 fn api_status() -> Json<StatusMsg> {
-    let msg = StatusMsg { 
-        status: 200, 
-        msg: String::from("API up and running.")
+    let msg = StatusMsg {
+        status: 200,
+        msg: String::from("API up and running."),
     };
     Json(msg)
 }
@@ -56,10 +65,22 @@ fn api_status() -> Json<StatusMsg> {
 
 #[get("/")]
 fn root() -> Json<StatusMsg> {
-    Json(StatusMsg{status: 200, msg: String::from("Rootsy bootsy.")})
+    Json(StatusMsg {
+        status: 200,
+        msg: String::from("Rootsy bootsy."),
+    })
 }
 
 
+// #[get("/retailer/<id>")]
+// fn get_retailer(conn: RetailerDbConn, id: usize) -> Result<Logs> {
+// 		Retailer::by_id(&*conn, id)
+// }
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello, api_status, root]).launch();
+    rocket::ignite()
+        .attach(RetailerDbConn::fairing())
+        .mount("/", routes![hello, api_status, root])
+        .launch();
 }
