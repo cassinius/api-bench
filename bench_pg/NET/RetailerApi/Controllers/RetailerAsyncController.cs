e768709c9d52d46f5d224a78c60f0ba2c03b55cf
Bernd Malle <bernd.malle@gmail.com>
2021-06-21 07:36:01 +0200

    also adapted sync controller to use utf8json library... not much difference new async results with corrected connection string, also not much difference ;-(

--- bench_pg/NET/RetailerApi/Controllers/RetailerAsyncController.cs
+++ bench_pg/NET/RetailerApi/Controllers/RetailerAsyncController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Utf8Json;
 
 using RetailerApi.Repositories;
 using RetailerApi.Models;
@@ -14,23 +15,25 @@ namespace RetailerApi.Controllers
     private readonly IRetailerAsyncRepository _retailerRepository;
     public RetailerAsyncController(IRetailerAsyncRepository retailerRepository)
     {
-        _retailerRepository = retailerRepository;
+      _retailerRepository = retailerRepository;
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Retailer>> GetRetailer(int id)
     {
-        var retailer = await _retailerRepository.Get(id);
-        if(retailer == null)
-            return NotFound();
-        return Ok(retailer);
+      var retailer = await _retailerRepository.Get(id);
+      if (retailer == null)
+        return NotFound();
+      var json = JsonSerializer.ToJsonString(retailer);
+      return Ok(json);
     }
 
     [HttpGet]
     public async Task<ActionResult<IAsyncEnumerable<Retailer>>> GetRetailers()
     {
-        var retailers = await _retailerRepository.GetAll();
-        return Ok(retailers);
+      var retailers = await _retailerRepository.GetAll();
+      var json = JsonSerializer.ToJsonString(retailers);
+      return Ok(json);
     }
   }
 }
