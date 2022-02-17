2d76ee32cab861235f8b695454a8dca636b96be1
Bernd Malle <bernd.malle@gmail.com>
2022-02-17 16:08:45 +0100

    updated arangodb .NET project and ran performance tests on Fedora / Arango 3.9 => about 10% slower than Arch / Arango 3.7

--- bench_arango/NET/min6api/Models/Retailer.cs
+++ bench_arango/NET/min6api/Models/Retailer.cs
@@ -2,12 +2,12 @@ namespace min6api.Models;
 
 public class Retailer {
   public int id {get; set;}
-  public string GSTIN {get; set;} = "";
-  public string Business_name {get; set;} = "";
-  public string Contact_person {get; set;} = "";
+  public string GSTIN {get; set;} = string.Empty;
+  public string Business_name {get; set;} = string.Empty;
+  public string Contact_person {get; set;} = string.Empty;
   public int Contact_number {get; set;}
-  public string Contact_address {get; set;} = "";
-  public string Contact_emailId {get; set;} = "";
-  public string Status {get; set;} = "";
+  public string Contact_address {get; set;} = string.Empty;
+  public string Contact_emailId {get; set;} = string.Empty;
+  public string Status {get; set;} = string.Empty;
   public int Outlet_limit {get; set;}
 }
---
+++ bench_arango/NET/min6api/README.md
@@ -0,0 +1,9 @@
+## Arango DB setup
+
+* Database: `retailer_api`
+* User: `retailer`
+* PW: `retailer`
+
+### URLs:
+
+* See swagger documentation for .NET 6 project...
