0caee98227e85718c40fcc245b20b61b0af73831
Bernd Malle <bernd.malle@gmail.com>
2022-07-02 14:08:18 +0200

    added Golang/fiber sub-project

--- bench_pg/Go/fiber/app.go
+++ bench_pg/Go/fiber/app.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"github.com/gofiber/fiber/v2"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/kataras/iris/v12"
 	"log"
 )
 
@@ -28,29 +28,25 @@ type Retailer struct {
  *
  */
 func main() {
-	app := iris.New()
-	//app.Logger().SetLevel("error")
-	//app.Use(recover.New())
-	//app.Use(logger.New())
-
 	connStr := "user=retailer password=retailer dbname=retailer_api sslmode=disable statement_cache_capacity=0"
 
 	dbPool, err := pgxpool.Connect(context.Background(), connStr)
-	//dbPool, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer dbPool.Close()
 
-	app.Get("/", func(ctx iris.Context) {
-		ctx.JSON(ApiResponse{
+	app := fiber.New()
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(ApiResponse{
 			Status:  200,
-			Message: "Go->Iris Retailer API up and running.",
+			Message: "Go->Fiber Retailer API up and running.",
 		})
 	})
 
-	app.Get("/retailers", func(ctx iris.Context) {
+	app.Get("/retailers", func(ctx *fiber.Ctx) error {
 		var retailers []Retailer
 		rows, err := dbPool.Query(context.Background(), "SELECT * FROM retailer")
 		if err != nil {
@@ -64,19 +60,19 @@ func main() {
 			}
 			retailers = append(retailers, retailer)
 		}
-		ctx.JSON(retailers)
+		return ctx.JSON(retailers)
 	})
 
-	app.Get("/retailer/{id}", func(ctx iris.Context) {
-		id := ctx.Params().Get("id")
+	app.Get("/retailer/:id", func(ctx *fiber.Ctx) error {
+		id := ctx.Params("id")
 		var retailer Retailer
 		row := dbPool.QueryRow(context.Background(), "SELECT * FROM retailer WHERE id = $1", id)
 		err := row.Scan(&retailer.Id, &retailer.GSTIN, &retailer.Business_name, &retailer.Contact_person, &retailer.Contact_number, &retailer.Constact_address, &retailer.Contact_emailId, &retailer.Status, &retailer.Outlet_limit)
 		if err != nil {
 			log.Fatal(err)
 		}
-		ctx.JSON(retailer)
+		return ctx.JSON(retailer)
 	})
 
-	app.Run(iris.Addr(":8000"))
+	app.Listen(":8000")
 }
