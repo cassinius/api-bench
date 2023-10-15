6e0fc75c5e2ce5e935ebf21e7b2c166f1cde15d6
Bernd Malle <bernd.malle@gmail.com>
2023-10-15 20:06:57 +0200

    added /retailer/{id} to .NET 8 npgsql

--- bench_pg/NET/8_npgsql/Program.cs
+++ bench_pg/NET/8_npgsql/Program.cs
@@ -17,14 +17,14 @@ app.MapGet("/retailers", async () =>
 {
     int rowsRead = 0;
     List<Retailer> retailers = new List<Retailer>();
-    
+
     // open the connection (every time!?)
     var conn = new NpgsqlConnection(connStr);
     await conn.OpenAsync();
 
     // get the (cached) connection
     // var conn = await npgSqlConn.GetConn();
-    
+
     // Retrieve all rows
     await using (var cmd = new NpgsqlCommand("SELECT * FROM retailer", conn))
     await using (var reader = await cmd.ExecuteReaderAsync())
@@ -43,16 +43,60 @@ app.MapGet("/retailers", async () =>
                 Status = reader.GetString(7),
                 OutletLimit = Convert.ToInt32(reader.GetDecimal(8)),
             });
-            
+
             rowsRead++;
         }
     }
 
     // Console.WriteLine($"Read {rowsRead} retailers from DB");
-    
+
     conn.Close();
 
     return retailers;
+});
+
+app.MapGet("/retailer/{id}", async (int id) =>
+{
+    int rowsRead = 0;
+    // List<Retailer> retailers = new List<Retailer>();
+    Retailer retailer = null;
+
+    // open the connection (every time!?)
+    var conn = new NpgsqlConnection(connStr);
+    await conn.OpenAsync();
+
+    // Retrieve retailer
+    await using (var cmd = new NpgsqlCommand("SELECT * FROM retailer WHERE id=@id LIMIT 1", conn))
+    {
+        cmd.Parameters.AddWithValue("id", id);
+
+        await using (var reader = await cmd.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                retailer = new Retailer()
+                {
+                    Id = Convert.ToInt32(reader.GetDecimal(0)),
+                    GSTIN = reader.GetString(1),
+                    BusinessName = reader.GetString(2),
+                    ContactPerson = reader.GetString(3),
+                    ContactNumber = Convert.ToInt32(reader.GetDecimal(4)),
+                    ContactAddress = reader.GetString(5),
+                    ContactEmailId = reader.GetString(6),
+                    Status = reader.GetString(7),
+                    OutletLimit = Convert.ToInt32(reader.GetDecimal(8)),
+                };
+
+                rowsRead++;
+            }
+        }
+    }
+
+    // Console.WriteLine($"Read {rowsRead} retailers from DB");
+
+    conn.Close();
+
+    return retailer;
 });
 
 app.Run("Retailer API running...");
