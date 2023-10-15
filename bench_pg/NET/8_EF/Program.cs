2e48df3696f6df69de175c3e218db9d5a76da906
Bernd Malle <bernd.malle@gmail.com>
2023-10-15 17:22:26 +0200

    added .NET Core 8 API with EF

---
+++ bench_pg/NET/8_EF/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Minapi7.Data;
+using Minapi7.Models;
+using Minapi7.Models.Responses;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connStr = builder.Configuration.GetConnectionString("EfPgConnection");
+builder.Services.AddDbContext<RetailerContext>(x => x.UseNpgsql(connStr));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => new APIStatusResponse
+{
+    Code = StatusCodes.Status200OK,
+    Message = "Retailer API up and running."
+})
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.MapGet("/retailers", async () =>
+{
+    using var scope = app.Services.CreateScope();
+    var ctx = scope.ServiceProvider.GetRequiredService<RetailerContext>();
+    var retailers = await ctx.Retailers.AsNoTracking().ToListAsync<Retailer>();
+    return retailers;
+});
+
+app.MapGet("/retailer/{id}", async (int id) =>
+{
+    using var scope = app.Services.CreateScope();
+    var ctx = scope.ServiceProvider.GetRequiredService<RetailerContext>();
+    var retailer = await ctx.Retailers.Where(r => r.Id == id).AsNoTracking().FirstOrDefaultAsync();
+    return retailer;
+});
+
+app.Run();
---
+++ bench_pg/NET/8_EF/README.md
@@ -0,0 +1,11 @@
+## Publish / release
+
+```shell
+dotnet publish -c Release --self-contained true -r linux-x64
+```
+
+then run
+
+```shell
+./bin/Release/net7.0/linux-x64/Core7_ef_miniapi
+```
